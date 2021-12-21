11112110e0c734a5847ecbeb109b14fa1bff22c6
Stephen B <stephen-bailly_student2021@wilder.school>
2021-12-21 17:20:09 +0100

    kind of workable form

---
+++ src/components/requestForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+const FORM_ENDPOINT = "";
+
+const ContactForm = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = () => {
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 100);
+  };
+  
+  
+  if (submitted) {
+    return (
+      <>
+        <h2>Merci beaucoup!</h2>
+        <div>Bobba vous recontactera pour les modalités de paiement.</div>
+      </>
+    );
+  }
+
+  return (
+    <form
+      action={FORM_ENDPOINT}
+      onSubmit={handleSubmit}
+      method=""
+      target=""
+    >
+
+
+      <h1>Réservation</h1><h4>Merci de nous indiquer vos préférences ainsi que tout commentaire utile à la réservation</h4>
+
+      <div>
+        <input type="text" placeholder="Your name" name="name" required />
+      </div>
+      <div>
+        <input type="email" placeholder="Email" name="email" required />
+      </div>
+        <div>
+          <select>
+            <option value="Peluche">Peluche</option>
+            <option value="Humanoïde">Humanoïde</option>
+            <option value="Droïde">Droïde</option>
+          </select>
+        </div>
+      <div>
+        <textarea placeholder="Comments" name="message" />
+      </div>
+      
+      <div>
+        <button type="submit">Send your request</button>
+      </div>
+    </form>
+  );
+};
+
+export default ContactForm;
