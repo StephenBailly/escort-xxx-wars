28fdf691de96087134dc75d2a58ad94bec5558d7
EmelineG <emeline-grondin_student2021@wilder.school>
2021-12-21 14:25:31 +0100

    navbar created

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react'
+import "../App.css"
+
+function Navbar() {
+    return (
+        <nav>
+            <ul className="list">
+               <NavLink to="./">
+                <li className="pages">Accueil</li>
+               </NavLink> 
+               <NavLink to="path to Escorts" >
+                <li className="pages">Escorts</li>
+               </NavLink>
+               <NavLink to="path to Experts" >
+                <li className="pages">Inscription</li>
+                </NavLink>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
