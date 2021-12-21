90e5724764528b04b6f50aba0a0c2ef53dc1960d
Florent-Lefevre <lefevre.flo@gmail.com>
2021-12-21 14:36:26 +0100

    card added

---
+++ components/cards.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react';
+import './cards.css';
+
+const Card = () => {
+const [card, setCard] = useState([])
+    useEffect(() => {
+        const getData = () => {
+            fetch("https://miadil.github.io/starwars-api/api")
+        }
+
+    }
+    )
+
+}
+
+
+
+export default Card;
