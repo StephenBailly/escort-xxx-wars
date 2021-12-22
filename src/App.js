5db1b7772384f1d138d1184963fcebf50fb9ee71
Iysha Barton <iysha.barton@gmail.com>
2021-12-22 12:33:03 +0100

    corrected

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import NavBar from './components/NavBar'
 import {Routes, Route} from 'react-router-dom'
 import Escorts from './components/Escorts'
+import Footer from './components/Footer'
 import Accueil from './components/Accueil'
-import Inscription from "./components/requestForm";
+import EscortsDetails from './components/EscortsDetails'
+import Inscription from "./components/requestForm.js";
 import "./App.css";
 import { useEffect } from "react";
 
@@ -14,10 +16,11 @@ function App() {
       <header className="App-header">
       <NavBar/>
 
-        <Routes>
+      <Routes>
           <Route path="/" element={<Accueil />} /> 
           <Route path="/Escorts" element={<Escorts />} />
-         <Route path="/requestForm" element={<requestForm />} />
+          <Route path="/Escorts/:id" element={<EscortsDetails/>} />
+          <Route path="requestForm" element={<Inscription />} />
         </Routes>
       </header>
 
